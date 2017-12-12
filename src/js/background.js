f046a621958ad16291a9d106faf546e3e47bf073
taeyonginayuta <taeyongtrack7195@gmail.com>
2017-12-12 18:10:44 +0800

    171212 stored settings values in chrome storage

--- src/js/background.js
+++ src/js/background.js
@@ -2,4 +2,47 @@
 
 angular.module('ISeeIHear', ['ngMaterial', 'ngMessages'])
 
-.controller('BgCtrl', function() {});
+
+// var newModality = '';
+
+.controller('BgCtrl', function($scope) {
+	
+	
+
+	// insert tutorials part in this button
+	$scope.howTo = function() {
+		alert("How To App 101 Button is clicked!");
+	}
+
+	// save settings in variables
+	$scope.saveSettings = function() {
+
+		// how to use chrome.storage.sync.get and chrome.storage.sync.set
+		chrome.storage.sync.get('opacity', function(settings) {
+			var newOpacity = 0;
+			newOpacity = parseInt($scope.opacity);
+			if(newOpacity) {
+				chrome.storage.sync.set({'opacity': newOpacity});
+				// alert(settings.opacity);
+			}
+		});
+
+		chrome.storage.sync.get('modality', function(settings) {
+			var newModality = $scope.modality;
+			if(newModality != undefined) {
+				chrome.storage.sync.set({'modality':  newModality});
+			}
+		});
+
+		alert($scope.opacity + "\n" + $scope.modality);
+
+		// Note: naseset naman 'yung opacity sa storage kaso hindi nagrereflect sa UI kapag sinasarado 'yung background.html
+		// Note: previous values ang lumalabas kapag napress ang save settings button naooverwrite lang siya if you press the save setting button again
+	}
+	
+	// edit bookmark
+	$scope.editBookmarks = function() {
+		alert("Edit Bookmark Button is clicked!");	
+	}
+
+});
