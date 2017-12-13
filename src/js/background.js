f177aec7049cfbf18e5ea592851a633ba4e994fc
taeyonginayuta <taeyongtrack7195@gmail.com>
2017-12-13 17:48:56 +0800

    171213 tries to make chrome storage work

--- src/js/background.js
+++ src/js/background.js
@@ -2,12 +2,16 @@
 
 angular.module('ISeeIHear', ['ngMaterial', 'ngMessages'])
 
-
-// var newModality = '';
-
 .controller('BgCtrl', function($scope) {
 	
-	
+	var SO = 0, MO, newOpacity = 0;
+	chrome.storage.local.get('opacity', function(settings) {
+		$('#storageO').text("Saved Opacity: " + settings.opacity);
+	});
+
+	chrome.storage.local.get('modality', function(settings) {
+		$('#storageM').text("Saved Modality: " + settings.modality);
+	});
 
 	// insert tutorials part in this button
 	$scope.howTo = function() {
@@ -18,31 +22,35 @@ angular.module('ISeeIHear', ['ngMaterial', 'ngMessages'])
 	$scope.saveSettings = function() {
 
 		// how to use chrome.storage.sync.get and chrome.storage.sync.set
-		chrome.storage.sync.get('opacity', function(settings) {
-			var newOpacity = 0;
-			newOpacity = parseInt($scope.opacity);
+		chrome.storage.local.get('opacity', function(settings) {
+			newOpacity = parseInt($scope.settings.opacity);
+			// if newOpacity is != 0
 			if(newOpacity) {
-				chrome.storage.sync.set({'opacity': newOpacity});
-				// alert(settings.opacity);
+				chrome.storage.local.set({'opacity': newOpacity});
 			}
 		});
 
-		chrome.storage.sync.get('modality', function(settings) {
-			var newModality = $scope.modality;
+		chrome.storage.local.get('modality', function(settings) {
+			var newModality = $scope.settings.modality;
+			// if newModality has a value
 			if(newModality != undefined) {
-				chrome.storage.sync.set({'modality':  newModality});
+				chrome.storage.local.set({'modality':  newModality});
 			}
 		});
 
-		alert($scope.opacity + "\n" + $scope.modality);
+		// alert(typeof(SO) + "\n" + typeof(MO));
+		// $scope.settings.opacity = 50;
+		// $scope.settings.modality = "Both";
 
-		// Note: naseset naman 'yung opacity sa storage kaso hindi nagrereflect sa UI kapag sinasarado 'yung background.html
+		// Note: naseset naman 'yung opacity sa storage kaso hindi nagrereflect sa UI kapag sinasarado 'yung background.html 
+		// 	Resolved: (?) Kasi narereflect siya sa UI pero hindi mismo dun sa options hays :(
 		// Note: previous values ang lumalabas kapag napress ang save settings button naooverwrite lang siya if you press the save setting button again
+		// Note: So it means na gumagana 'yung chrome storage :)
 	}
 	
 	// edit bookmark
 	$scope.editBookmarks = function() {
 		alert("Edit Bookmark Button is clicked!");	
-	}
-
+	}	
+	
 });
