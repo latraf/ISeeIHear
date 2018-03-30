9b95976578dd5848973d04fd2e391d4d91359374
taeyonginayuta <taeyongtrack7195@gmail.com>
2018-03-31 01:24:33 +0800

    connected webgazer but not yet functioning

--- src/js/popup.js
+++ src/js/popup.js
@@ -9,34 +9,38 @@ $(function() {
 
 /* redirects to a new tab when "How To"is clicked */
 document.addEventListener('DOMContentLoaded', function() {
-	document.getElementById("howTo").addEventListener("click", howToTab);
+	document.getElementById('howTo').addEventListener('click', howToTab);
 });
 
 function howToTab() {
-	// [!!!] create separate url on how to use the extension
-	chrome.tabs.create({'url': 'src/toolbar.html'}, function(tab) {
+	chrome.tabs.create({'url': 'src/howto.html'}, function(tab) {
 	});
 }
 
 /* saves modality into chrome storage when button is clicked */
 document.addEventListener('DOMContentLoaded', function() {
-	document.getElementById("saveBtn").addEventListener("click", saveSettings);
+	document.getElementById('saveBtn').addEventListener('click', saveSettings);
 });
 
-var modeOut = "", mode = 0;
+var modeOut = '', mode = 0;
 
 function saveSettings() {
 	mode = $('input[name=radio-1]:checked').val();
 	switch(mode) {
-		case '1': modeOut = "Gaze"; break;
-		case '2': modeOut = "Voice"; break;
-		case '3': modeOut = "Both"; break;
+		case '1': modeOut = 'Gaze'; break;
+		case '2': modeOut = 'Voice'; break;
+		case '3': modeOut = 'Both'; break;
 	}
 	// $("#mode").html("Mode: " + modeOut);
 
 	// console.log(modeOut);
-	alert("Saved! Mode: " + modeOut);
+	console.log('Saved! Mode: ' + modeOut);
 	chrome.storage.local.set({'mode': mode});
+
+	if(mode == '1' | mode == '3') {
+		connectWebGazer();
+		console.log('connecting webgazer...');
+	}
 }
 
 /* loads previously saved modality */
@@ -44,17 +48,17 @@ function loadSettings() {
 	chrome.storage.local.get('mode', function(result) {
 		mode = result.mode;
 		switch(mode) {
-			case '1': modeOut = "Gaze"; break;
-			case '2': modeOut = "Voice"; break;
-			case '3': modeOut = "Both"; break;
+			case '1': modeOut = 'Gaze'; break;
+			case '2': modeOut = 'Voice'; break;
+			case '3': modeOut = 'Both'; break;
 		}
 		// $("#mode").html("Mode: " + modeOut);
 		// alert(modeOut);
 		
 		switch(mode) {
-			case "1": $('#radio-1').prop("checked", true); break;
-			case "2": $('#radio-2').prop("checked", true); break;
-			case "3": $('#radio-3').prop("checked", true); break;
+			case '1': $('#radio-1').prop('checked', true); break;
+			case '2': $('#radio-2').prop('checked', true); break;
+			case '3': $('#radio-3').prop('checked', true); break;
 		}
 	});
 }
@@ -62,4 +66,11 @@ function loadSettings() {
 /* calls loading function everytime popup.html loads*/
 window.onload = function() {
 	loadSettings();
+}
+
+/* function to connect webgazer.js to the extension */
+function connectWebGazer() {
+	chrome.tabs.executeScript({file: 'src/js_ext/webgazer.js'}, function() {
+		chrome.tabs.executeScript({file: 'src/js/gaze-controls.js'});
+	});
 }
