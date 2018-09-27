55c4fb27a993822b1ea907c154c8c46ec16e814d
taeyonginayuta <taeyongtrack7195@gmail.com>
2018-09-27 14:02:19 -0700

    [GAZE] WG: add tab id in execute script

--- src/js/popup.js
+++ src/js/popup.js
@@ -137,7 +137,7 @@ function connectBoth(tab_id) {
 	// chrome.tabs.executeScript(tab_id, {file: ''});   // script that will enable voice-controls
 }
 
-function removeControls() {
+function removeControls(tab_id) {
 	var data = {
 		'gaze_mode' : false,
 		'voice_mode' : false,
@@ -147,7 +147,7 @@ function removeControls() {
 	console.log('Modes are turned off.');
 	setData(data);
 	// chrome.tabs.executeScript(tab_id, {file: 'src/js_ext/webgazer.js'}, function() {
-		chrome.tabs.executeScript({file: 'src/js/gaze-controls-off2.js'});
+		chrome.tabs.executeScript(tab_id, {file: 'src/js/gaze-controls-off2.js'});
 	// });
 }
 
@@ -164,12 +164,13 @@ window.onload = function() {
 
 	// console.log('Current Tab ID: ' + curr_tab_id);
 	// console.log('Current Window ID: ' + curr_window_id);
-	
-	chrome.tabs.executeScript(curr_tab_id, {file: 'src/js_ext/jquery-3.1.1.min.js'});
+	if(curr_tab_id) {
+		chrome.tabs.executeScript(curr_tab_id, {file: 'src/js_ext/jquery-3.1.1.min.js'});
+		chrome.tabs.executeScript(curr_tab_id, {file: 'src/js/gaze-controls-off.js'});
+	}
 	// chrome.tabs.executeScript(curr_tab_id, {file: 'src/js_ext/webgazer.js'}, function() {
 	// 	chrome.tabs.executeScript(curr_tab_id, {code: 'webgazer.pause()'});
 	// });
-	chrome.tabs.executeScript(curr_tab_id, {file: 'src/js/gaze-controls-off.js'});
 	// chrome.tabs.executeScript({file: 'src/js_ext/jquery-ui.min.js'});
 	loadSettings();
 	console.log("popup loaded!");
