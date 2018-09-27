42ed3aa39704b05f00ddfb703fa216b2389ca735
taeyonginayuta <taeyongtrack7195@gmail.com>
2018-09-27 13:51:58 -0700

    [VOICE] add speech recog not yet working

--- src/js/voice.js
+++ src/js/voice.js
@@ -5,9 +5,37 @@ document.documentElement.style.height = '100%';
 document.documentElement.style.width = '100%';
 
 var voice_input = document.createElement('input');
+var voice_btn = document.createElement('button');
 
 voice_input.setAttribute('id', 'voice_input');
 voice_input.type = 'text';
 voice_input.disabled = true;
 
+voice_btn.setAttribute('id', 'voice_btn');
+
+document.body.appendChild(voice_btn);
 document.body.appendChild(voice_input);
+
+/* reference: https://www.sitepoint.com/introducing-web-speech-api/ */
+window.SpeechRecognition = window.SpeechRecognition  || window.webkitSpeechRecognition || null;
+
+if(window.SpeechRecognition !== null) {
+	console.log('has speech recog yaaay');
+
+	var recognizer = new window.SpeechRecognition();
+	var text = document.getElementById('voice_input');
+
+	recognizer.continuous = true;
+
+	// recognizer.onstart = function(event) {
+	// 	text.value = '';
+
+	// 	for(var i=event.resultIndex; i<event.results.length; i++) {
+	// 		text.value = event.results[i][0].transcript;
+	// 	}
+
+	// 	recognizer.start();
+	// }
+
+}
+
