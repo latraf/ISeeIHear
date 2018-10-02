131e815907db532ad7e6866a31089acac3f1b68e
taeyonginayuta <taeyongtrack7195@gmail.com>
2018-10-01 23:12:29 -0700

    [VOICE] can do speech-to-text

--- src/js/voice.js
+++ src/js/voice.js
@@ -13,8 +13,8 @@ voice_input.disabled = true;
 
 voice_btn.setAttribute('id', 'voice_btn');
 
-document.body.appendChild(voice_btn);
 document.body.appendChild(voice_input);
+document.body.appendChild(voice_btn);
 
 /* reference: https://www.sitepoint.com/introducing-web-speech-api/ */
 window.SpeechRecognition = window.SpeechRecognition  || window.webkitSpeechRecognition || null;
@@ -23,19 +23,52 @@ if(window.SpeechRecognition !== null) {
 	console.log('has speech recog yaaay');
 
 	var recognizer = new window.SpeechRecognition();
-	var text = document.getElementById('voice_input');
+	// var text = document.getElementById('voice_input');
 
 	recognizer.continuous = true;
+	// recognizer.start();
 
+	// text.value = '';
+
+	// for(var i=event.resultIndex; i<event.results.length; i++) {
+	// 	text.value = event.results[i][0].transcript;
+	// }
 	// recognizer.onstart = function(event) {
-	// 	text.value = '';
+	// 	voice_input.value = 'onstart';
+	// 	console.log(event.resultIndex);
+	// 	// recognizer.start();
+	// 	// console.log(event.results.length);
+	// }
 
-	// 	for(var i=event.resultIndex; i<event.results.length; i++) {
-	// 		text.value = event.results[i][0].transcript;
-	// 	}
+	recognizer.onresult = function(event) {
+		console.log('onresult');
+
+
+		for(var i=event.resultIndex; i<event.results.length; i++) {
+			if(event.results[i].isFinal) {
+				voice_input.value = event.results[i][0].transcript;
+			}
+			else {
+				voice_input.value += event.results[i][0].transcript
+			}
+		}
+
+		if(voice_input.value === 'hello') console.log('recognized: ' + voice_input.value);
 
 	// 	recognizer.start();
-	// }
+	}
+
+	voice_btn.addEventListener('click', function() {
+		console.log('activate voice recognition');
+
+		try {
+			recognizer.start();
+			console.log('recog starting');
+		}
+		catch(ex) {
+			console.log(ex.message);
+		}
+	});
 
 }
 
